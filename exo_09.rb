960dedb4e34f5c7bde4bb158ca2f0862d49807df
Lucie Caillat <lucie.caillat@gmail.com>
2021-10-05 09:29:10 +0200

    copie des exo de j6

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, quel est ton nom de famille ?"
+print "--> "
+last_name = gets.chomp
+puts "Et quel est ton prénom ? "
+print "--> "
+user_name = gets.chomp 
+puts " Salut #{ user_name} #{last_name} !"
---
+++ exo_10.rb
@@ -0,0 +1,4 @@
+puts "En quelle année es tu née ?"
+print "--> "
+year_of_bird = gets.chomp.to_i
+puts "Tu avais donc #{ 2017 - year_of_bird } ans en 2017."
